a279149c43504d31147ecb4cb4b92fb50813d2e4
Eerieee <111144811+Eerieee@users.noreply.github.com>
2022-08-12 12:48:41 -0500

    Update clientbridgeadmin.lua

--- clientbridgeadmin.lua
+++ clientbridgeadmin.lua
@@ -66,8 +66,8 @@ local CMDS = {
 -- [ RS ] --
 
 game:GetService("RunService").RenderStepped:Connect(function()
-    if LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") and Spinning == true then
-        LP.Character:FindFirstChild("HumanoidRootPart").CFrame = LP.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.Angles(0, math.rad(1.5), 0)
+    if game.Players.LocalPlayer and LP.Character and LP.Character:FindFirstChild("HumanoidRootPart") and Spinning == true then
+        LP.Character:FindFirstChild("HumanoidRootPart").CFrame = LP.Character:FindFirstChild("HumanoidRootPart").CFrame * CFrame.Angles(0, math.rad(3), 0)
     end
 end)
 
